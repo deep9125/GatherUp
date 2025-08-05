f5940ba6eb2950df76d654624b96a9f949bdf1f6
Rathod Deep <rathoddeep711@gmail.com>
2025-08-05 12:23:05 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,7 @@
 const Login = () => {
   return(<>
     <form action="POST" className="login">
+      <h1>Login</h1>
         <label htmlFor="Username">Username</label>
         <input type="text" name="Username"/>
         <label htmlFor="Password">Password</label>
